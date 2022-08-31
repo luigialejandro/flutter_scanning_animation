79a1913cd7878623a78faabe618c8ebafde96348
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-08-30 20:46:41 -0500

    Building a good portion of the code of the project. The most relevant part is the developing of the ScannerAnimation class which handles and continuously updates the linear gradient positioned values to create the scanning animation. Additionally, I am going to require a ScannerScreen widget to harbour the widget to be scanned.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_scanning_animation/scanner_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,35 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return CupertinoApp(
+      title: 'Photo Scanner',
+      debugShowCheckedModeBanner: false,
+      theme: CupertinoThemeData(
+        brightness: Brightness.dark,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child:
-              Container()), // This trailing comma makes auto-formatting nicer for build methods.
+      home: ScannerScreen(),
     );
   }
 }
---
+++ lib/scanner_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScannerScreen extends StatefulWidget {
+  ScannerScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ScannerScreen> createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends State<ScannerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
